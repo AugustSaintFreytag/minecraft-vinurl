Plompi/VinURL

backport to 1.20.1

src/main/java/com/vinurl/client/VinURLClient.java
@@ -9,11 +9,8 @@ import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.NbtComponent;
-import net.minecraft.item.Item;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
@@ -40,9 +37,9 @@ public class VinURLClient implements ClientModInitializer {
 		Commands.register();
 		ClientEvent.register();
 
-		ItemTooltipCallback.EVENT.register((ItemStack stack, Item.TooltipContext context, TooltipType type, List<Text> lines) -> {
+		ItemTooltipCallback.EVENT.register((ItemStack stack, TooltipContext context, List<Text> lines) -> {
 			if (stack.getItem() == CUSTOM_RECORD && CONFIG.showDescription()) {
-				String fileName = AudioHandler.hashURL(stack.getOrDefault(DataComponentTypes.CUSTOM_DATA, NbtComponent.DEFAULT).copyNbt().get(URL_KEY));
+				String fileName = AudioHandler.hashURL(stack.getOrCreateNbt().getString(URL_KEY));
 
 				if (fileName.isEmpty()) {return;}
 

AugustSaintFreytag/minecraft-vinurl

src/main/java/com/vinurl/client/VinURLClient.java
@@ -9,9 +9,6 @@ import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.NbtComponent;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
@@ -34,14 +31,13 @@ public class VinURLClient implements ClientModInitializer {
 		Commands.register();
 		ClientEvent.register();
 
-		ItemTooltipCallback.EVENT.register((stack, context, type, lines) -> {
+		ItemTooltipCallback.EVENT.register((stack, context, lines) -> {
 			if (stack.getItem() != CUSTOM_RECORD) {return;}
 
 			lines.remove(Text.translatable("item.vinurl.custom_record.desc").formatted(Formatting.GRAY));
 
 			if (CONFIG.showDescription()) {
-				NbtCompound nbt = stack.getOrDefault(DataComponentTypes.CUSTOM_DATA, NbtComponent.DEFAULT).copyNbt();
-				String fileName = SoundManager.hashURL(nbt.get(URL_KEY));
+				String fileName = SoundManager.hashURL(stack.getOrCreateNbt().getString(URL_KEY));
 
 				if (!fileName.isEmpty()) {
 					lines.add(Text.literal(SoundManager.getDescription(fileName)).formatted(Formatting.GRAY));