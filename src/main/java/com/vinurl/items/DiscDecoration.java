ae4cefe945d16d7b75dba41d9d1df51a78316170
August Saint Freytag <me@augustfreytag.com>
2025-12-12 02:40:12 +0100

    Fix undyed label default color

--- src/main/java/com/vinurl/items/DiscDecoration.java
+++ src/main/java/com/vinurl/items/DiscDecoration.java
@@ -8,12 +8,18 @@ public record DiscDecoration(int coreColor, int sideColor, int labelColor, boole
 
 	public static final int DEFAULT_CORE_COLOR = 0x383838;
 	public static final int DEFAULT_SIDE_COLOR = 0x2d2d2d;
-	public static final int DEFAULT_LABEL_COLOR = 0xdddddd;
+	public static final int DEFAULT_LABEL_COLOR = 0xefefef;
 
 	private static final String DECORATION_KEY = "Decoration";
 	private static final String CORE_KEY = "Core";
 	private static final String SIDE_KEY = "Side";
 	private static final String LABEL_KEY = "Label";
+
+	// State
+
+	public boolean isCustomized() {
+		return hasLabel || coreColor != DEFAULT_CORE_COLOR || sideColor != DEFAULT_SIDE_COLOR || labelColor != DEFAULT_LABEL_COLOR;
+	}
 
 	// Defaults
 
@@ -35,6 +41,11 @@ public record DiscDecoration(int coreColor, int sideColor, int labelColor, boole
 		var hasLabel = decorationNbt.contains(LABEL_KEY);
 		var label = hasLabel ? decorationNbt.getInt(LABEL_KEY) : DEFAULT_LABEL_COLOR;
 
+		if (hasLabel && label == 0) {
+			// If label exists but has not been dyed, use default label color.
+			label = DEFAULT_LABEL_COLOR;
+		}
+
 		return new DiscDecoration(core, side, label, hasLabel);
 	}
 
@@ -47,7 +58,9 @@ public record DiscDecoration(int coreColor, int sideColor, int labelColor, boole
 		decorationNbt.putInt(SIDE_KEY, sideColor);
 
 		if (hasLabel) {
-			decorationNbt.putInt(LABEL_KEY, labelColor);
+			// Write default label color if label is in default form.
+			var resolvedLabelColor = labelColor == 0 ? DEFAULT_LABEL_COLOR : labelColor;
+			decorationNbt.putInt(LABEL_KEY, resolvedLabelColor);
 		} else {
 			decorationNbt.remove(LABEL_KEY);
 		}
